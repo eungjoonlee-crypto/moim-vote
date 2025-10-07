0d2362824add0dc299789bed509a247ae9699b34
Jun <jun@example.com>
2025-10-07 11:49:55 +0900

    feat: 검색 버튼에서 돋보기 아이콘 제거

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -233,7 +233,6 @@ const Index = () => {
               onClick={() => handleSearch(searchQuery)}
               className="px-4 py-2 h-14 text-lg"
             >
-              <Search className="w-5 h-5 mr-2" />
               검색
             </Button>
           </div>
